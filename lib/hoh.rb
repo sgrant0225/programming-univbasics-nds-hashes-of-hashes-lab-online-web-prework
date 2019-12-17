5d842cb5a036e960fdffd4742f1cec31bb28d6af
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-17 00:53:24 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,8 +46,7 @@ new_hash = {
             :label => "Genus",
             :sub_category => {
               :label => "Species",
-              :sub_category => { nil
-              }
+              :sub_category => {}
             }
           }
         }
