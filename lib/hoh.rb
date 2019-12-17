459b290fb9d191438350a1e80357ed163a682dc3
Sharnell Grant <sharnellgrant@gmail.com>
2019-12-17 00:46:05 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,7 +32,25 @@
 
 def naming_system
   # Remember:
-  #  Kingdom
+new_hash = {
+  :label => "Kingdom",
+  :sub_category => {
+    :label => "Phylum",
+    :sub_category => {
+      :label => "Class",
+      :sub_category => {
+        :label => "Order",
+        :sub_category => {
+          :label => "Family",
+          :sub_category => {
+            :label => "Genus",
+          }
+        }
+      }
+    }
+  }
+}
+    
   #  Phylum
   #  Class
   #  Order
